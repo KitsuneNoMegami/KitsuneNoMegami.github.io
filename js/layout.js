c4619691c00ab132481b587b14b2041da58384fa
Beth <e_mops@hotmail.com>
2026-05-22 14:46:15 +0200

    [GUESTBOOK] ajout d'un guestbook

--- js/layout.js
+++ js/layout.js
@@ -221,3 +221,14 @@ document.addEventListener(
     document.getElementById("quote").innerHTML = quotesList[Math.floor(Math.random() * quotesList.length)];
   }
 );
+
+//CommentBox
+if (!window.hcb_user) { hcb_user = {}; }
+(function () {
+  var s = document.createElement("script"),
+    l = hcb_user.PAGE || ("" + window.location).replace(/'/g, "%27"),
+    h = "https://www.htmlcommentbox.com";
+  s.setAttribute("type", "text/javascript");
+  s.setAttribute("src", h + "/jread?page=" + encodeURIComponent(l).replace("+", "%2B")  + "&opts=16798");
+  if (typeof s != "undefined") document.getElementsByTagName("head")[0].appendChild(s);
+})();
